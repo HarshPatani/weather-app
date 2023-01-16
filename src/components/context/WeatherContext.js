47a8feb179866f7a02a328cdf76bf9a8f7a7501f
Harsh Patani <96504206+HarshPatani@users.noreply.github.com>
2023-01-16 12:41:36 +0530

    initial commit

---
+++ src/components/context/WeatherContext.js
@@ -0,0 +1,41 @@
+import { React, createContext, useState } from "react";
+// import useLocalStorage from "use-local-storage";
+
+export const WeatherContext = createContext();
+
+const WeatherContextProvider = ({ children }) => {
+  const [tableData, setTableData] = useState([]);
+  const [cities, setCities] = useState([
+    {
+      city: "London",
+      clicked: false,
+    },
+    {
+      city: "New York",
+      clicked: false,
+    },
+    {
+      city: "Los Angeles",
+      clicked: false,
+    },
+    {
+      city: "Las Vegas",
+      clicked: false,
+    },
+  ]);
+
+  return (
+    <WeatherContext.Provider
+      value={{
+        tableData,
+        setTableData,
+        cities,
+        setCities,
+      }}
+    >
+      {children}
+    </WeatherContext.Provider>
+  );
+};
+
+export default WeatherContextProvider;
